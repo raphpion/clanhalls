9edaf87da6b90949c2d617659911380923f16c6d
Raphaël Pion <raphael.pion@outlook.com>
2024-07-28 18:31:17 -0400

    Add Windows dev startup script

---
+++ backend/scripts/dev.ps1
@@ -0,0 +1,11 @@
+$workDir = [System.IO.DirectoryInfo]::new($PSScriptRoot).Parent.Parent.FullName
+
+# Commands
+$compose = "docker compose -f docker-compose.local.yml up -d"
+$migrate = "npm run migrate -w backend"
+$startBackend = "npm run dev -w backend"
+$startFrontend = "npm run dev -w frontend"
+
+# Start processes
+Start-Process -FilePath "powershell.exe" -ArgumentList "-NoExit", "-Command", "cd $workDir; $compose; $migrate; $startBackend;"
+Start-Process -FilePath "powershell.exe" -ArgumentList "-NoExit", "-Command", "cd $workDir; $startFrontend;"
