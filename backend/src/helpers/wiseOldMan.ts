d2891aa5287e02b0fb89caa07fb287cd45423cfc
Raphaël Pion <raphael.pion@outlook.com>
2024-10-02 11:27:25 -0400

    Remove console.log

--- backend/src/helpers/wiseOldMan.ts
+++ backend/src/helpers/wiseOldMan.ts
@@ -13,9 +13,6 @@ export async function withSafeWiseOldMan<T>(
       if (error.name === 'RateLimitError') {
         // Retry in 60 seconds if the request fails because of rate limiting
         // See https://docs.wiseoldman.net/#rate-limits--api-keys
-        console.log(
-          'WiseOldMan has rate limited us, retrying in 60 seconds...',
-        );
         await new Promise((r) => setTimeout(r, 60000));
         continue;
       }
