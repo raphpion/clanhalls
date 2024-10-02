0362170a3411baa1fc8b692b5dce5642aa364150
Raphaël Pion <raphael.pion@outlook.com>
2024-10-01 22:18:01 -0400

    Management hell * Remove obsolete 'version' field * Use cmd.exe for scripts for legacy environment support

--- scripts/dev.ps1
+++ scripts/dev.ps1
@@ -1,11 +1,9 @@
 $workDir = [System.IO.DirectoryInfo]::new($PSScriptRoot).Parent.FullName
 
-# Commands
 $compose = "docker compose -f docker-compose.dev.yml up -d"
 $migrate = "npm run migrate -w backend"
 $startBackend = "npm run dev -w backend"
 $startFrontend = "npm run dev -w frontend"
 
-# Start processes
-Start-Process -FilePath "powershell.exe" -ArgumentList "-NoExit", "-Command", "cd $workDir; $compose; $migrate; $startBackend;"
-Start-Process -FilePath "powershell.exe" -ArgumentList "-NoExit", "-Command", "cd $workDir; $startFrontend;"
+Start-Process -FilePath "cmd.exe" -ArgumentList "/K", "cd /d $workDir && $compose && $migrate && $startBackend"
+Start-Process -FilePath "cmd.exe" -ArgumentList "/K", "cd /d $workDir && $startFrontend"
